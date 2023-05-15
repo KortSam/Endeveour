7d4587cf4f9b37ef09defe36f987973f3d59208f
KortSam <samkort@hotmail.nl>
2023-05-15 16:07:07 +0200

    added finished project

---
+++ services/rijksmuseum.tsx
@@ -0,0 +1,42 @@
+export interface Artwork {
+  id: string;
+  objectNumber: string;
+  title: string;
+  titles: string;
+  webImage: { url: string };
+  dating: {
+    presentingDate: string;
+  };
+  label: {
+    description: string;
+  };
+}
+
+export interface ArtworkDetail extends Artwork {
+  longTitle: string;
+}
+
+//normaly I wouldn't store this here but just for easy access it is here!
+const RIJKS_API_KEY = '2nxMGcgf';
+
+
+
+export const searchArtworks = async (
+  query: string,
+  sortOption: string
+): Promise<Artwork[]> => {
+  let url = `https://www.rijksmuseum.nl/api/en/collection?key=${RIJKS_API_KEY}&ps=100`;
+  if (sortOption) url += `&s=${sortOption}`;
+  if (query) url += `&q=${query}`;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.artObjects;
+};
+
+export const getArtworkDetails = async (id: string): Promise<ArtworkDetail> => {
+  const res = await fetch(
+    `https://www.rijksmuseum.nl/api/nl/collection/${id}?key=${RIJKS_API_KEY}`
+  );
+  const data = await res.json();
+  return data.artObject;
+};
